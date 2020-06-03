db9f7629d2ac784ee0ee6bcbb07edfb5847ba2b7
or shilon <or shilon>
2020-06-03 19:55:29 +0300

    adasd

--- Ex03.GarageLogic/FuelVehicleManager.cs
+++ Ex03.GarageLogic/FuelVehicleManager.cs
@@ -10,7 +10,8 @@ namespace Ex03.GarageLogic
     {
         public static float GetFuelLeft(Vehicle i_vehicle)
         {
-            return i_vehicle.m_FuelOrBatteryLeft; //testhfjhyfhfh
+            return i_vehicle.m_FuelOrBatteryLeft;
+            Console.WriteLine();
         }
 
         public static float GetMaxFuelPossible(Vehicle i_vehicle)
