5df627cd6a0a50b6d59497c59297cc0041f62c12
or shilon <or shilon>
2020-06-05 17:59:03 +0300

    makeing vehicleGarage

--- Ex03.GarageLogic/ElectricCar.cs
+++ Ex03.GarageLogic/ElectricCar.cs
@@ -12,15 +12,11 @@ namespace Ex03.GarageLogic
         internal int m_NumOfDoors;
         internal const float m_MaxBattery = 2.1f;
         internal const byte m_NumOfWheels = 4;
-        public ElectricCar(string i_VehicleModel, string i_LicencePlate, float i_EnergyLeft, string i_Color, int i_NumOfDoors)
+        public ElectricCar(string i_VehicleModel, string i_LicencePlate, float i_EnergyLeft, string i_Color, int i_NumOfDoors) :
+            base(i_VehicleModel, i_LicencePlate, i_EnergyLeft, m_MaxBattery, m_NumOfWheels)
         {
             m_Color = i_Color;
             m_NumOfDoors = i_NumOfDoors;
-            m_VehicleModel = i_VehicleModel;
-            m_LicencePlate = i_LicencePlate;
-            m_FuelOrBatteryLeft = i_EnergyLeft;//
-            m_Wheels = new Wheel[m_NumOfWheels];
-            m_MaxFuelOrBattery = m_MaxBattery;
         }
     }
 }
--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -13,6 +13,15 @@ namespace Ex03.GarageLogic
         internal float m_FuelOrBatteryLeft;
         internal float m_MaxFuelOrBattery;
         internal Wheel[] m_Wheels;
+
+        public Vehicle(string i_VehicleModel, string i_LicencePlate, float i_FuelOrBatteryLeft, float i_MaxFuelOrBattery, int i_NumOfWheels)
+        {
+            m_VehicleModel = i_VehicleModel;
+            m_LicencePlate = i_LicencePlate;
+            m_FuelOrBatteryLeft = i_FuelOrBatteryLeft;
+            m_MaxFuelOrBattery = i_MaxFuelOrBattery;
+            m_Wheels = new Wheel[i_NumOfWheels];
+        }
     }
 
 
